2518c8cc7a5186bc6b638142cd9d4e6352498661
adman40 <adman40@cs.utexas.edu>
2024-12-20 12:27:44 -0700

    init debugging

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,17 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Extension "github-productivity-tracker" is now active!');
-
+    console.log('Activating github-productivity-tracker extension...');
+    
     let disposable = vscode.commands.registerCommand('githubProductivityTracker.helloWorld', () => {
+        console.log('Executing Hello World command...');
         vscode.window.showInformationMessage('Hello World from github-productivity-tracker!');
     });
 
     context.subscriptions.push(disposable);
+    console.log('Command registered: githubProductivityTracker.helloWorld');
 }
 
-export function deactivate() {}
+export function deactivate() {
+    console.log('Deactivating github-productivity-tracker extension...');
+}
